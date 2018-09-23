2b9970b12e705cd78daba9af3c381486b0bd9c05
Armin Ulrich <armin@nifty.at>
2018-09-23 13:23:30 +0200

    w

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ Route::get('/', function () {
     $streamers->push(new \App\Streamer('getaclue_1'));
     $streamers->push(new \App\Streamer('patwalls'));
     $streamers->push(new \App\Streamer('pugs0n'));
-    $streamers->push(new \App\Streamer('arminulrich'));
+    //$streamers->push(new \App\Streamer('arminulrich'));
     $streamers->push(new \App\Streamer('pretzelhands'));
     //$streamers->push(new \App\Streamer('levelsio'));
     //$streamers->push(new \App\Streamer('andrey_azimov'));
-    //$streamers->push(new \App\Streamer('h0h0h0'));
+    $streamers->push(new \App\Streamer('h0h0h0'));
     //$streamers->push(new \App\Streamer('lenilsonjr_'));
     //$streamers->push(new \App\Streamer('swizec'));
 
