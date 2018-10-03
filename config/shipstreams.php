6aba6ee33df21f1c24e24936842e029ca9e20db3
arminulrich <armin@nifty.at>
2018-10-03 11:31:51 +0200

    disable ph header

--- config/shipstreams.php
+++ config/shipstreams.php
@@ -2,7 +2,7 @@
 return [
     "ph-alert" =>
         [
-            "show" => true,
+            "show" => false,
             "url" => "https://www.producthunt.com/posts/shipstreams"
         ]
 ];
