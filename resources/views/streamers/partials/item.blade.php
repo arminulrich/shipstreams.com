f6becdff064813054cd66e23f688fd7307e1f388
arminulrich <armin@nifty.at>
2018-10-03 18:15:47 +0200

    switch twitch url to detail

--- resources/views/streamers/partials/item.blade.php
+++ resources/views/streamers/partials/item.blade.php
@@ -4,11 +4,11 @@
 <div class="col-12 col-md-6 col-lg-4">
     <div class="streamer @if($streamer->is_online) is-streaming @endif">
         <div class="streamer__image-wrap">
-            <a target="_blank" href="{{$streamer->twitch_url}}">
+            <a  href="{{$streamer->shipstreams_url}}">
                 <img class="streamer__image" src="{{$streamer->twitch_profile_image_url}}"/>
             </a>
         </div>
-        <a target="_blank" href="{{$streamer->twitch_url}}" class="streamer__name">{{$streamer->twitch_displayname}}</a>
+        <a  href="{{$streamer->shipstreams_url}}" class="streamer__name">{{$streamer->twitch_displayname}}</a>
         @if($streamer->twitch_profile_description)
             <div class="streamer__description">{{$streamer->twitch_profile_description}}</div>
         @endif
