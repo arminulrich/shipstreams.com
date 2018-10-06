db177dee83a4974c017c6df54ca2ed7ffddb0b0d
arminulrich <armin@nifty.at>
2018-10-06 09:42:36 +0200

    add makerwidget

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,5 +29,20 @@
     @include('layouts.partials.footer')
 </div>
 <script src="{{mix('js/app.js')}}"></script>
+
+<script>
+    (function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+                position: "left",
+                widget: "r9uqyc6u0hyqxsbw-0tyc1jcafgzz68zc-m4go6a6ahubralb1"
+            })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
+</script>
 </body>
 </html>
